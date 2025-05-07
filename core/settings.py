c898b3817b83a35919fe3a1532284e9e75195a9f
ashraf940 <ashrafbugti69@gmail.com>
2025-05-07 14:56:47 +0500

    update

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    
 ]
 EXTERNAL_APPS = [
     'account',
--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(max_length=15)
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=500)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self):
+        return self.car_name
